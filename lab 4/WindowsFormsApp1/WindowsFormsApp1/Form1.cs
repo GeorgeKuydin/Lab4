dd5458486cfbd9b8b360055c8f1b350f07aa841c
GeorgeKuydin <125215800+GeorgeKuydin@users.noreply.github.com>
2023-04-10 22:38:21 +0300

    Add files via upload

---
+++ lab 4/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GameProcessBuilder builder = new GameProcessBuilder();
+            GameProcessDirector director = new GameProcessDirector(builder);
+
+            // Construct game process with different configurations
+            director.Construct("Alice", 1, true);
+            GameProcess gameProcess1 = builder.GetGameProcess();
+            gameProcess1.Display();
+
+            director.Construct("Bob", 2, false);
+            GameProcess gameProcess2 = builder.GetGameProcess();
+            gameProcess2.Display();
+
+            director.Construct("Charlie", 3, true);
+            GameProcess gameProcess3 = builder.GetGameProcess();
+            gameProcess3.Display();
+        }
+    }
+}
---
+++ lab 4/WindowsFormsApp1/WindowsFormsApp1/GameProcess.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class GameProcess
+    {
+        private string _playerName;
+        private int _difficultyLevel;
+        private bool _soundEnabled;
+
+        public GameProcess(string playerName, int difficultyLevel, bool soundEnabled)
+        {
+            _playerName = playerName;
+            _difficultyLevel = difficultyLevel;
+            _soundEnabled = soundEnabled;
+        }
+
+        public void Display()
+        {
+            MessageBox.Show("Game process:");
+            MessageBox.Show("Player name: " + _playerName);
+            MessageBox.Show("Difficulty level: " + _difficultyLevel);
+            MessageBox.Show("Sound enabled: " + (_soundEnabled ? "Yes" : "No"));
+        }
+    }
+
+    // Builder interface
+    interface IGameProcessBuilder
+    {
+        void SetPlayerName(string playerName);
+        void SetDifficultyLevel(int difficultyLevel);
+        void SetSoundEnabled(bool soundEnabled);
+        GameProcess GetGameProcess();
+    }
+
+    // Concrete builder class
+    class GameProcessBuilder : IGameProcessBuilder
+    {
+        private string _playerName;
+        private int _difficultyLevel;
+        private bool _soundEnabled;
+
+        public void SetPlayerName(string playerName)
+        {
+            _playerName = playerName;
+        }
+
+        public void SetDifficultyLevel(int difficultyLevel)
+        {
+            _difficultyLevel = difficultyLevel;
+        }
+
+        public void SetSoundEnabled(bool soundEnabled)
+        {
+            _soundEnabled = soundEnabled;
+        }
+
+        public GameProcess GetGameProcess()
+        {
+            return new GameProcess(_playerName, _difficultyLevel, _soundEnabled);
+        }
+    }
+
+    // Director class
+    class GameProcessDirector
+    {
+        private IGameProcessBuilder _builder;
+
+        public GameProcessDirector(IGameProcessBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void Construct(string playerName, int difficultyLevel, bool soundEnabled)
+        {
+            _builder.SetPlayerName(playerName);
+            _builder.SetDifficultyLevel(difficultyLevel);
+            _builder.SetSoundEnabled(soundEnabled);
+        }
+    }
+}
